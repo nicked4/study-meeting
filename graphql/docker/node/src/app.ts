1c5990524cb177c200d28e80de2579855efad29c
NMinakami <82941006+NMinakami@users.noreply.github.com>
2022-12-29 23:27:04 +0900

    [Add] node.jsのTypeScript対応

--- graphql/docker/node/src/app.ts
+++ graphql/docker/node/src/app.ts
@@ -4,31 +4,31 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index'); // 変更箇所
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('views'));  // 変更箇所
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join('public')));  // 変更箇所
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req: any, res: any, next: any) {  // 変更箇所
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: any, req: any, res: any, next: any) {  // 変更箇所
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- graphql/docker/node/src/routes/index.ts
+++ graphql/docker/node/src/routes/index.ts
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: any, res: any, next: any) {  // 変更箇所
   res.render('index', { title: 'Express' });
 });
 
