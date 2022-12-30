efbae68826bb373786c8bd2c2de4700fdc02605a
NMinakami <82941006+NMinakami@users.noreply.github.com>
2022-12-30 23:57:41 +0900

    [Add] Apollo Server 導入

--- graphql/docker/node/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req: any, res: any, next: any) {  // 変更箇所
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
